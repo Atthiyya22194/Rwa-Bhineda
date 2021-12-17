5bb1d12c3c2061c8feccb22f8a94d1d297bf0df9
Atthiyya <atthiyya@gmail.com>
2021-12-17 12:20:03 +0700

    Adding Assets from Khoiri

--- Assets/Scripts/FollowThePath.cs
+++ Assets/Scripts/FollowThePath.cs
@@ -4,102 +4,33 @@ using UnityEngine;
 
 public class FollowThePath : MonoBehaviour
 {
-    //board
-    public Transform[] waypoint;
-    //convert waypoint 1D ke 2D di void Start
-    public Vector3[,] waypoints;
-    public int boardSize = 7;
+    public Transform[] waypoints;
     [SerializeField] private float moveSpeed = 1f;
     [HideInInspector] public int waypointIndex = 0;
-    [SerializeField] private int waypointX = 0;
-    [SerializeField] private int waypointY = 0;
     public bool moveAllowed = false;
-    public Vector2 targetDir;
-    public int targetCounter;
 
     // Start is called before the first frame update
     void Start()
     {
-        //uncomment this
-        transform.position = waypoint[waypointIndex].transform.position;
-        //itterate waypoint dan assign ke waypoints
-        waypoints = new Vector3[boardSize, boardSize];
-        for (int x = 0; x < boardSize; x++)
-        {
-            for (int y = 0; y < boardSize; y++)
-            {
-                waypoints[x, y] = waypoint[x + y * boardSize].position;
-            }
-        }
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
-    }
-
-#if UNITY_EDITOR
-    //this code block only compiled on unity editor, wont be taken on build
-    public Vector2 TestMoveDir;
-    public int TestCounter;
-    [ContextMenu("TestMoveButton")]
-    void TestMoveButton()
-    {
-        MoveButton(TestMoveDir, TestCounter);
-    }
-#endif
-
-    public void MoveButton(Vector2 dir, int counter)
-    {
-        //ini debug only, jangan lupa diapus atau bungkus ama directive #if UNITY_EDITOR
-        Debug.Log($"dir: {dir}");
-        Debug.Log($"counter: {counter}");
-
-        //store the counter to local variable
-        targetCounter = counter;
-        //store the direction to local variable
-        targetDir = dir;
-        //move allow di set true
+        if (moveAllowed)
+            Move();
     }
 
     private void Move()
     {
-        if (targetCounter > 0)
+        if(waypointIndex <= waypoints.Length - 1)
         {
-            int nextX = waypointX + (int)targetDir.x;
-            int nextY = waypointY + (int)targetDir.y;
-            //kalau nextX < 0, nextX dipaksa 0
-            if (nextX < 0)
-            {
-                nextX = 0;
-            }
-            //kalau nextX > boardSize-1, nextX dipaksa boardsize-1
-            else if (nextX > boardSize - 1)
-            {
-                nextX = boardSize - 1;
-            }
-            if (nextY < 0)
-            {
-                nextY = 0;
-            }
-            else if (nextY > boardSize - 1)
-            {
-                nextY = boardSize - 1;
-            }
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
 
-            Vector3 nextWaypoint = waypoints[nextX, nextY];
-            transform.position = Vector3.MoveTowards(transform.position, nextWaypoint, moveSpeed * Time.deltaTime); //moveSpeed * Time.deltaTime is move distance 
-
-            //https://docs.unity3d.com/ScriptReference/Vector3.Equals.html
-            //use the == operator to test two vectors for approximate equality.
-            if (transform.position == nextWaypoint)
+            if(transform.position == waypoints[waypointIndex].transform.position)
             {
-                //TODO: check tile effect
-                //waypointIndex += 1;
-                waypointX += (int)targetDir.x;
-                waypointY += (int)targetDir.y;
-                targetCounter--;
+                waypointIndex += 1;
             }
         }
     }
--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -3,11 +3,14 @@ using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
-    private static GameObject player1;
+
+    private static GameObject whoWinsTextShadow, player1MoveText, player2MoveText;
+
+    private static GameObject player1, player2;
 
     public static int diceSideThrown = 0;
     public static int player1StartWaypoint = 0;
-   // public static int player2StartWaypoint = 0;
+    public static int player2StartWaypoint = 0;
 
     public static bool gameOver = false;
 
@@ -15,9 +18,19 @@ public class GameControl : MonoBehaviour
     void Start()
     {
 
+        whoWinsTextShadow = GameObject.Find("WhoWinsText");
+        player1MoveText = GameObject.Find("Player1MoveText");
+        player2MoveText = GameObject.Find("Player2MoveText");
+
         player1 = GameObject.Find("Player1");
+        player2 = GameObject.Find("Player2");
 
         player1.GetComponent<FollowThePath>().moveAllowed = false;
+        player2.GetComponent<FollowThePath>().moveAllowed = false;
+
+        whoWinsTextShadow.gameObject.SetActive(false);
+        player1MoveText.gameObject.SetActive(true);
+        player2MoveText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,10 +39,32 @@ public class GameControl : MonoBehaviour
         if (player1.GetComponent<FollowThePath>().waypointIndex > player1StartWaypoint + diceSideThrown)
         {
             player1.GetComponent<FollowThePath>().moveAllowed = false;
+            player1MoveText.gameObject.SetActive(false);
+            player2MoveText.gameObject.SetActive(true);
             player1StartWaypoint = player1.GetComponent<FollowThePath>().waypointIndex - 1;
         }
+
+        if (player2.GetComponent<FollowThePath>().waypointIndex > player2StartWaypoint + diceSideThrown)
+        {
+            player2.GetComponent<FollowThePath>().moveAllowed = false;
+            player2MoveText.gameObject.SetActive(false);
+            player1MoveText.gameObject.SetActive(true);
+            player2StartWaypoint = player2.GetComponent<FollowThePath>().waypointIndex - 1;
+        }
+
         if (player1.GetComponent<FollowThePath>().waypointIndex == player1.GetComponent<FollowThePath>().waypoints.Length)
         {
+            whoWinsTextShadow.gameObject.SetActive(true);
+            whoWinsTextShadow.GetComponent<Text>().text = "Player 1 Wins";
+            gameOver = true;
+        }
+
+        if (player2.GetComponent<FollowThePath>().waypointIndex == player2.GetComponent<FollowThePath>().waypoints.Length)
+        {
+            whoWinsTextShadow.gameObject.SetActive(true);
+            player1MoveText.gameObject.SetActive(false);
+            player2MoveText.gameObject.SetActive(false);
+            whoWinsTextShadow.GetComponent<Text>().text = "Player 2 Wins";
             gameOver = true;
         }
     }
@@ -40,6 +75,10 @@ public class GameControl : MonoBehaviour
         {
             case 1:
                 player1.GetComponent<FollowThePath>().moveAllowed = true;
+                break;
+
+            case 2:
+                player2.GetComponent<FollowThePath>().moveAllowed = true;
                 break;
         }
     }
