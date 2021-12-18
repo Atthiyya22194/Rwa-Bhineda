bc72fbc8d9abee37097be76560f8137359c38c36
Atthiyya <atthiyya@gmail.com>
2021-12-19 02:16:07 +0700

    Add Win and Lose Screen

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -3,34 +3,29 @@ using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
-    public GameObject whoWinsTextShadow;
-
-    public GameObject player1;
-
+    public GameObject player;
     public static int diceSideThrown = 0;
     //public static Vector2 movementDir = Vector2.Zero;
     public static int player1StartWaypoint = 0;
-
     public static bool gameOver = false;
 
     // Use this for initialization
     void Start()
     {
-        player1 = GameObject.Find("Player1");
-
-        player1.GetComponent<FollowThePath>().moveAllowed = false;
+        player = GameObject.Find("Player");
+        player.GetComponent<PlayerMovement>().moveAllowed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player1.GetComponent<FollowThePath>().waypointIndex > player1StartWaypoint + diceSideThrown)
+        if (player.GetComponent<PlayerMovement>().waypointIndex > player1StartWaypoint + diceSideThrown)
         {
-            player1.GetComponent<FollowThePath>().moveAllowed = false;
-            player1StartWaypoint = player1.GetComponent<FollowThePath>().waypointIndex - 1;
+            player.GetComponent<PlayerMovement>().moveAllowed = false;
+            player1StartWaypoint = player.GetComponent<PlayerMovement>().waypointIndex - 1;
         }
 
-        if (player1.GetComponent<FollowThePath>().waypointIndex == player1.GetComponent<FollowThePath>().waypoints.Length)
+        if (player.GetComponent<PlayerMovement>().waypointIndex == player.GetComponent<PlayerMovement>().waypoints.Length)
         {
             gameOver = true;
         }
