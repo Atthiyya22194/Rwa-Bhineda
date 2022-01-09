a38861ff821a3827679382831b0efa10df26a72c
Khoiri_Amarulloh <Mkhoiri037@gmail.com>
2022-01-09 14:40:34 +0700

    Add sfx

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,20 +6,9 @@ using UnityEngine.Audio;
 public class SoundManager : MonoBehaviour
 {
     public AudioSource sfxButton;
-    public AudioSource sfxHurt;
-    public AudioSource sfxPick;
+
     public void PlayButton()
     {
         sfxButton.Play();
-    }
-
-    public void PlayHurt()
-    {
-        sfxHurt.Play();
-    }
-
-    public void PlayPick()
-    {
-        sfxPick.Play();
     }
 }
