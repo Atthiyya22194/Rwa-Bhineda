aa81521a864b711db5a58a1672e28f3f974fde66
Atthiyya <atthiyya@gmail.com>
2022-01-09 14:52:19 +0700

    Add audio code

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
+    private SoundManager sfxTrap;
+
     void OnTriggerEnter2D(Collider2D coll)
     {
         HealthBar.health -= 20;
         Destroy(gameObject);
+        FindObjectOfType<SoundManager>().PlayButton();
     }
 }
